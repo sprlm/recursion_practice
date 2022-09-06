4fde1417fbf8511dcb295c6e688b269ce50f7993
Matthew <matthewtoro17@gmail.com>
2022-09-06 22:32:53 +0800

    Create recursive function for fibonacci sequence

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,12 +1,31 @@
 let fibs = (length) => {
   if (length < 1) return "Please enter a length greater than 0.";
 
-  let output = [0, 1, 1];
+  let output = [0, 1];
 
-  for (let i = 3; i < num; i++) {
+  for (let i = 2; i < length; i++) {
     output.push(output[i-1] + output[i-2]);
   }
 
-  return output.splice(0, length);
+   console.log(output.splice(0, length));
 };
 
+let fibsRec = (length, output = [0, 1]) => {
+  if (length < 1) return "Please enter a length greater than 0.";
+
+  if (output.length !== length && length > 2) {
+    output.push(output[output.length-1] + output[output.length-2])
+    fibsRec(length, output);
+  } else {
+    console.log(output.splice(0, length));
+  }
+};
+
+fibs(8);
+fibsRec(8);
+
+fibs(1);
+fibsRec(1);
+
+fibs(20);
+fibsRec(20);
